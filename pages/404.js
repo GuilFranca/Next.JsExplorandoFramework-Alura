79a0f24285b5028d8f46f8c8c9e5260dda37c6d3
Guilherme França <guilfranca27@gmail.com>
2026-01-20 15:45:13 -0300

    Aula 3 - 02 Onde colocar meu CSS?

--- pages/404.js
+++ pages/404.js
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Page404() {
 
-    return(
+    return (
         <div>
             <h1>Você se perdeu e caiu na página 404 :O</h1>
             <Link href="/">Ir para Home</Link>
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,34 @@
+import React from "react";
 import Link from "next/link";
 // import Link from '../src/components/Link';
+
+function Title({ children, as }) {
+
+    const Tag = as;
+
+    return (
+        // Permite adicionar mais de uma tag sem a necessidade de uma div pai
+        <React.Fragment> 
+            <Tag>
+                {children}
+            </Tag>
+            <style jsx>{` 
+                ${as} {
+                    color: red;
+                    font-family: sans-serif;
+                }
+            `}</style>
+        </React.Fragment>
+    );
+
+}
 
 export default function HomePage() {
 
     return (
         <div>
-            <h1>Alura Cases - Home Page</h1>
+            <Title as="p">Alura Cases - Home Page</Title>
+            {/* <h1>Alura Cases - Home Page</h1> */}
             <Link href="/faq" passHref>Ir para o FAQ</Link>
             {/* <Link href="/faq">
                 Ir para faq
