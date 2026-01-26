3620aedbe0fe641d60bf83fd8c3695ca1b8c4968
Guilherme França <guilfranca27@gmail.com>
2026-01-26 16:06:54 -0300

    Aula 5 - 03 Preparando o ambiente: Recursos da aula 5

--- pages/faq.js
+++ pages/faq.js
@@ -1,51 +1,24 @@
-import Head from 'next/head';
-import Link from "next/link";
+// faq.js
+import FAQScreen from '../src/screens/FAQScreen';
 
-// export async function getServerSideProps() {
-// Reccarrega toda vez que a página é renderizada
+export default FAQScreen; 
 
 export async function getStaticProps() {
-    // Recarrega somente no build quando utilizamos o comando yarn export, mas recarrega toda vez que a página rederiza quando estamos no yarn dev
-
-    console.log('Função de fetch carregada🚀');
-
-    const FAQ_API_URL = 'https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/e0b5ce160567e6443d3ef13e651ad41b3e5cc20e/alura-cases-faq.json';
+    console.log('Em modo DEV, sempre roda! A cada acesso')
+    console.log('Roda SOMENTE em build time')
+    const FAQ_API_URL = 'https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json';
     const faq = await fetch(FAQ_API_URL)
         .then((respostaDoServidor) => {
             return respostaDoServidor.json();
         })
         .then((resposta) => {
-            // console.log(resposta);
             return resposta;
         });
 
     return {
-        props: {
-            exemploProps: 'Exemplo de props🚀',
-            faq,
-        }
-    }
-}
-
-export default function FAQPage({ faq }) {
-    return (
-        <div>
-            <Head>
-                <title>FAQ - Alura Cases Campanha</title>
-            </Head>
-            <h1>Alura Cases - Página de Perguntas FAQ</h1>
-            <Link href="/" passHref>Ir para o Home</Link>
-            <ul>
-                {faq.map(({ answer, question }) => (
-                    <li key={question}>
-                        <article>
-                            <h2>{question}</h2>
-                            <p>{answer}</p>
-                        </article>
-                    </li>
-                ))}
-            </ul>
-        </div>
-    );
-
-}
+      props: {
+          qualquercoisa: 'que eu passar aqui',
+          faq,
+      },
+    };
+}  
